0ccf2f4945f877c2bf2804338cdb884827ef596e
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-08-17 22:03:41 +0530

    DB Connected and server running

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Database Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log("MongoDB connection failure", error.message);
+  }
+};
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  connectDB();
+  console.log("server is running on " + port);
+});
